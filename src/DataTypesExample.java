d7a59e23ab706279d7f5e5f6b97563b4dc40fff7
ghghboy1457 <csm120064@gmail.com>
2024-09-11 17:19:16 +0900

    처음 커밋

---
+++ src/DataTypesExample.java
@@ -0,0 +1,7 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+       char letter ='A';
+       int cone = letter;
+       System.out.println("the cone" + letter + "is : " + cone);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age=20;
+        double temperature=36.5;
+        boolean isStudrnt =true;
+        char c=65;
+
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println(isStudrnt);
+        System.out.println(c);
+    }
+}
+
